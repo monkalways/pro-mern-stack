ca419eca5a93f10ebfe6770918fdc6ff69482ffe
Wei Wei (EPS) <monkalways@gmail.com>
2018-06-18 14:53:36 -0600

    Add effort filter

--- server/server.js
+++ server/server.js
@@ -16,20 +16,34 @@ app.get('/api/issues', (req, res) => {
   if (req.query.status) {
     filter.status = req.query.status;
   }
-  db.collection('issues').find(filter).toArray().then((issues) => {
-    res.json(issues);
-  });
+  if (req.query.effort_lte || req.query.effort_gte) filter.effort = {};
+  if (req.query.effort_lte) {
+    filter.effort.$lte = parseInt(req.query.effort_lte, 10);
+  }
+  if (req.query.effort_gte) {
+    filter.effort.$gte = parseInt(req.query.effort_gte, 10);
+  }
+  db.collection('issues')
+    .find(filter)
+    .toArray()
+    .then((issues) => {
+      res.json(issues);
+    });
 });
 
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
   newIssue.status = 'New';
   newIssue.created = new Date();
-  db.collection('issues').insertOne(newIssue).then((doc) => {
-    db.collection('issues').findOne({ _id: doc.insertedId }).then((issue) => {
-      res.json(issue);
+  db.collection('issues')
+    .insertOne(newIssue)
+    .then((doc) => {
+      db.collection('issues')
+        .findOne({ _id: doc.insertedId })
+        .then((issue) => {
+          res.json(issue);
+        });
     });
-  });
 });
 
 app.listen(3000, () => {
